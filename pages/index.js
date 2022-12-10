d17c538c275d8e8ab3e8606c2330714633505913
sajib925 <sajibahmed1004@gmail.com>
2022-12-10 12:47:36 +0600

    clean home page file

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,7 @@
-import Button from "../Components/Button/Button";
-import Container from "../Components/Container/Container";
-import {H1,H2, H3, H4, H5, LgPara, MdPara, SmPara} from "./../styles/ShareStyles";
 const Home = () => {
   return (
     <>
-      <Container>
-        <H1>Hello World !</H1>
-        <H2>Hello World !</H2>
-        <H3>Hello World !</H3>
-        <H4>Hello World !</H4>
-        <H5>Hello World !</H5>
-        <H5 className="small_heading">Hello World !</H5>
-        <SmPara>this is sm para</SmPara>
-        <MdPara>this is md para</MdPara>
-        <LgPara>this is lg para</LgPara>
-        <Button variant="primary" href="/" text="button" />
-        <Button variant="transparent" text="button" />
-        <Button variant="smBtn" text="button" />
-      </Container>
+      
     </>
   );
 };
