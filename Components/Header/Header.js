133dd322bfbeb2d0798a21d9459990bcbe4c6103
sajib925 <sajibahmed1004@gmail.com>
2022-12-11 19:32:31 +0600

    first commit

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import StyledHeader from "./StyledHeader";
 import Container from "./../Container/Container";
 import Image from "next/image";
 import logo from "../../Images/logo/Logo.svg";
 import Navbar from "../Navbar/Navbar";
-
+import Cart from "./../icons/Cart";
+import Button from "../Button/Button";
+import MobileBtn from "./../icons/mobileBtn";
 
 const Header = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const MobileMenuClick = () => {
+    setMobileMenu((mobileMenu) => !mobileMenu);
+  };
+
+  
+
   return (
     <StyledHeader>
       <Container>
@@ -16,10 +27,29 @@ const Header = () => {
               <Image src={logo} alt="logo" />
             </a>
           </div>
-          <div className="middle">
+          <div
+            className={mobileMenu ? "middle open__menu" : "middle close__menu"}
+          >
             <Navbar />
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <div className="cart">
+              <div className="cart__icon">
+                <Cart />
+              </div>
+            </div>
+            <div className="btn">
+              <div className="btn__signIn">
+                <Button variant="smBtn" text="Sign In" />
+              </div>
+              <div
+                className="btn__mobile"
+                onClick={MobileMenuClick}
+              >
+                <MobileBtn />
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </StyledHeader>
--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,38 +1,80 @@
-import Image from "next/image";
 import React from "react";
 import StyledNavbar from "./StyledNavbar";
+import Search from "./../icons/Search";
+import DownArrow from "./../icons/DownArrow";
+import Link from "next/link";
+import DATA from './../../Data/Data';
+import Button from './../Button/Button';
 
 const Navbar = () => {
   return (
     <StyledNavbar>
       <ul className="nav">
-        <li className="nav__item">
-          <a href="/" className="nav__links">
-            <span className="nav__links__text">Occasions</span>
-            {/* <Image src={downArrow} alt="down svg" className="nav__links__svg" /> */}
-          </a>
+        <li className="nav__item item menu">
+          <Link href="/" className="nav__link link">
+            <span className="nav__link__text">Occasions</span>
+            <span className="nav__link__arrow__icon">
+              <DownArrow />
+            </span>
+          </Link>
+
+          <ul className="sub__menu">
+            {DATA.header.Occasions.map(({to, name}, index) => {
+              return (
+                <li className="sub__menu__item item" key={index}>
+                  <Link href={to} className="sub__menu__link link">
+                    {name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </li>
-        <li className="nav__item">
-          <a href="/" className="nav__links">
-            <span className="nav__links__text">Flowers</span>
-            {/* <Image src={downArrow} alt="down svg" className="nav__links__svg" /> */}
-          </a>
+
+        <li className="nav__item item menu">
+          <Link href="/" className="nav__link link">
+            <span className="nav__link__text">Flowers</span>
+            <span className="nav__link__arrow__icon">
+              <DownArrow />
+            </span>
+          </Link>
+
+          <ul className="sub__menu">
+            {DATA.header.Flowers.map(({to, name}, index) => {
+              return (
+                <li className="sub__menu__item item" key={index}>
+                  <Link href={to} className="sub__menu__link link">
+                    {name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </li>
-        <li className="nav__item">
-          <a href="/" className="nav__links">
+
+        <li className="nav__item item">
+          <Link href="/" className="nav__link link">
             About us
-          </a>
+          </Link>
         </li>
-        <li className="nav__item">
-          <a href="/" className="nav__links">
+
+        <li className="nav__item item">
+          <Link href="/" className="nav__link link">
             Contact us
-          </a>
+          </Link>
         </li>
-        <li className="nav__item">
-          <a href="/" className="nav__links">
-            {/* <Image src={search} alt="search" className="nav__links__svg" /> */}
-            <span className="nav__links__text">Search</span>
-          </a>
+
+        <li className="nav__item item">
+          <Link href="/" className="nav__link link">
+            <span className="nav__link__search__icon">
+              <Search />
+            </span>
+            <span className="nav__link__text">Search</span>
+          </Link>
+        </li>
+
+        <li className="nav__item item signIn__btn">
+          <Button variant="smBtn" text="Sign In" />
         </li>
       </ul>
     </StyledNavbar>
